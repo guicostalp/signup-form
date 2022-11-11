5b1df34e91a0b1a268b58b6baaa466d6cbd68d61
Gui Costa <gui.costalp@gmail.com>
2022-11-12 02:58:13 +1300

    More data validation

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ const pw = document.querySelector('#password');
 const pwC = document.querySelector('#confirm-password');
 const pwError = document.querySelector('#password-error');
 const pwCError = document.querySelector('#confirm-password-error');
+const button = document.querySelector('.submit-button')
 
 pwC.addEventListener("input", function (event) {
     if (pwC.value !== pw.value) {
       pwCError.textContent = 'Passwords do not match';
+      button.disabled=true;      
     } else {
       pwCError.textContent = '';
+      button.disabled=false;   
     }
 });
+
