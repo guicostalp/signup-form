2b13ef0a9968a00c38d312e77a27da3fa41f6cd3
Gui Costa <gui.costalp@gmail.com>
2022-11-11 19:15:00 +1300

    More data validation

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Javascript is alive");
+const pw = document.querySelector('#password');
+const pwC = document.querySelector('#confirm-password');
+const pwError = document.querySelector('#password-error');
+const pwCError = document.querySelector('#confirm-password-error');
+
+pwC.addEventListener("input", function (event) {
+    if (pwC.value !== pw.value) {
+      pwCError.textContent = 'Passwords do not match';
+    } else {
+      pwCError.textContent = '';
+    }
+});
